5fc3d9c9a43a865dd93e3f76e9159c795bfe8884
Maxsem <max.semegen@gmail.com>
2020-07-03 21:45:28 -0400

    updated styling so background image covers entire screen; created Project component, portfolio section complete

--- react-portfolio/src/components/Profile/Profile.js
+++ react-portfolio/src/components/Profile/Profile.js
@@ -12,7 +12,7 @@ const Profile = function () {
                     <p>My LinkedIn profile can be found <a href="https://linkedin.com/in/maxime-semegen-b38521b2/">here</a></p> */}
             </div>
             <div className="col-12 col-lg-9">
-                <p>My name is Max Semegen.  I am a former financial Advisor who decided to become a web developer because I enjoy solving technical problems and creating things with my own hands.</p>
+                <p>My name is Max Semegen.  I am a former Financial Advisor who decided to become a web developer because I enjoy solving technical problems and creating things with my own hands.</p>
                 <p>I am proficient in both front end and back end technologies.  I have built basic web pages using only HTML, CSS and JavaScript, as well as full stack applications using Node and Express back ends connecting to MySQL and MongoDB databases.  I have also built front ends using React (which was used to make this portfolio!)</p>
                 <p>Outside of work, I enjoy reading and biking.  I am also a bit of a gamer, and currently enjoy Magic: the Gathering and JRPGs.</p>
             </div>
